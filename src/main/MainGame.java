06ca784e41068bb2a7cfda3bf594ecbb295afb69
choejieun <wldms5221@icloud.com>
2023-12-04 22:23:04 +0900

    feat(MainGame) : 게임을 실행 할 메인 클래스

---
+++ src/main/MainGame.java
@@ -0,0 +1,12 @@
+package src.main;
+
+import src.cardgame.PlayGame;
+
+public class MainGame {
+
+        public static void main(String[] args) {
+            PlayGame.playGame();
+        }
+
+}
+
