335765e9f051fef65aeccd3143c6bb9aa76b31ba
choejieun <wldms5221@icloud.com>
2023-12-04 21:35:58 +0900

    feat(Deck) : 1 ~ 8 까지의 숫자를 갖는 카드를 각각 3장씩 생성하는 함수 구현

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,9 @@
 package src;
 
 public class Card {
+    private final int number;
+
+    public Card(int number) {
+        this.number = number;
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,20 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+//    public static List<Card>> deck() {
+//        List<Card> cards = initializeDeck();
+//        return cards;
+//    }
+    private static List<Card> initializeDeck() {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 1; j < 9; j++) {
+                cards.add(new Card(j));
+            }
+        }
+        return cards;
+    }
 }
